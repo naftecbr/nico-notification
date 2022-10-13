d84dd91d8b075fe0a8ff4d9f02852159711b2438
naftecbr <101275431+naftecbr@users.noreply.github.com>
2022-10-13 19:09:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
+    fetch("https://api.telegram.org/bot5424423676:AAF40gthymMhZyta1JMSGK52n91kwY6_lA4/sendMessage?chat_id=-714399706&parse_mode=Markdown&text=Test"); 
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
