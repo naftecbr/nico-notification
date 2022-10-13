ee2b1216192483a668fe2594c1b2c46f9222c221
naftecbr <101275431+naftecbr@users.noreply.github.com>
2022-10-13 19:30:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ var http = require('http');
 var fetch = require('node-fetch');
 
 http.createServer(function (req, res) {
-    fetch("https://api.telegram.org/bot5424423676:AAF40gthymMhZyta1JMSGK52n91kwY6_lA4/sendMessage?chat_id=-714399706&parse_mode=Markdown&text=Test"); 
+    const messages = [
+        "Babycitita lembre da pilulitaaa te amo para sempre sempre",
+        "Pilula timeeee ❤️",
+        "👶 ⛔ 💊",
+        "Pilula timeeee ❤️",
+        "👶 ⛔ 💊",
+        "PiLULA 13 Presidente!",
+        "N'oubliez pas de prendre la pilule mon cher",        
+    ];
+    fetch(BOT_CODE+messages[new Date().getDay()]); 
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
